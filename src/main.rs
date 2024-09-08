cea51c4f092a487f5c0e206fdab6071086ca576a
Rommel Samanez <rsamanez@gmail.com>
2024-09-08 14:22:20 -0500

    Using minifb_fonts to draw Text in 80x25 resolution screen

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // Using minifb to manage window presentation to emulate the CPU Screeen
 use minifb::*;
+use minifb_fonts::*;
 
-
-const SCREEN_WIDTH: usize = 640;
-const SCREEN_HEIGHT: usize = 480;
+//TEXT RESOLUTION 80X25 USING 6X8 FONT SIZE
+const SCREEN_WIDTH: usize = 480;
+const SCREEN_HEIGHT: usize = 200;
 
 
 fn main() {
@@ -16,8 +17,7 @@ fn main() {
     let buffer_width = SCREEN_WIDTH;
     let buffer_height = SCREEN_HEIGHT;
     let window_scale = Scale::X2;
-    let x_buffer_width = 640;
-    let x_buffer_height = 480;
+ 
     
     let mut color = from_u8_rgb(0, 0, 0);
     let mut buffer: Vec<u32> = vec![color; buffer_width * buffer_height];
@@ -25,29 +25,22 @@ fn main() {
     let mut window = Window::new("Test", window_width, window_height, WindowOptions { scale: window_scale, ..WindowOptions::default() }).unwrap();
     window.set_position(75, 20);
     
-    let mut offset: usize;
-    let mut alfa: f32 = 0.0;
-    let mut delta: f32 = 0.0;
-    let mut rx: f32 = 0.0;
-    let mut ry: f32 = 0.0;
-    let mut rY:u32 = 0;
-    let mut rX:u32 = 0;
+    
     color = from_u8_rgb(250, 0, 0);
-    rx = 200.0;//*alfa.sin()+240.0;
-    ry = 200.0;//*alfa.sin()+240.0;
-    while window.is_open(){
-        alfa = alfa+0.01;
-        if alfa >= 3.1415*2.0 {
-            alfa = 0.0;
-            delta = delta+0.5;
-        }
-        ry = 200.0*(alfa+delta).cos()+240.0;
-        rx = 200.0*alfa.sin()+240.0;
-        rY = ry as u32;
-        rX = rx as u32;
-        offset = (rX+rY*640) as usize;
-        
-        buffer[offset] = color;
-        window.update_with_buffer(&buffer, x_buffer_width, x_buffer_height).unwrap();
+
+    let mut text = font6x8::new_renderer(buffer_width, buffer_height, color);
+    text.draw_text(&mut buffer, 0, 0, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+    text.draw_text(&mut buffer, 0, 8, "12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+    text.draw_text(&mut buffer, 0, 16, "AAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCDDDDDDDDDDDDDDEEEEEEEEEEE");
+    text.set_color(0x00_ff_00);
+    text.draw_text(&mut buffer, 10, 180, "Press ESC to exit");
+    text.draw_text(&mut buffer, 10, 30, "A");
+    text.draw_text(&mut buffer, 16, 30, "B");
+    text.draw_text(&mut buffer, 22, 30, "C");
+    text.draw_text(&mut buffer, 10, 38, "D");
+    text.draw_text(&mut buffer, 16, 38, "E");
+    text.draw_text(&mut buffer, 22, 38, "F");
+    while window.is_open() && !window.is_key_down(minifb::Key::Escape){
+        window.update_with_buffer(&buffer, buffer_width, buffer_height).unwrap();
     }
 }
