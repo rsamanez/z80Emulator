994fc8307b795dfd40fb50ab126dcc3397df0db7
Rommel Samanez <rsamanez@gmail.com>
2024-09-16 12:24:02 -0500

    first test using FLTK library to manage GUI elements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use fltk::{ app, button, window, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let a = app::App::default();
+    let mut win = window::Window::default().with_size(400,300).with_label("Hola Mundo...");
+    let mut btn = button::Button::default().with_size(80,30).center_of_parent().with_label("Cleck here");
+    win.end();
+    win.show();
+    btn.set_callback(move |b| {
+        b.set_label("Clecked.");
+        win.set_label("Nuevo Window Title");
+    });
+    a.run().unwrap();
 }
