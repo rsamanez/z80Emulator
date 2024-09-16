54c22c3d9cf0a3c982ccd2f193e5b9da0bcb9f28
Rommel Samanez <rsamanez@gmail.com>
2024-09-16 12:30:04 -0500

    Sample using minifb lybrary

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
-use fltk::{ app, button, window, prelude::*};
+use minifb::{Key, ScaleMode, Window, WindowOptions};
+
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
 
 fn main() {
-    let a = app::App::default();
-    let mut win = window::Window::default().with_size(400,300).with_label("Hola Mundo...");
-    let mut btn = button::Button::default().with_size(80,30).center_of_parent().with_label("Cleck here");
-    win.end();
-    win.show();
-    btn.set_callback(move |b| {
-        b.set_label("Clecked.");
-        win.set_label("Nuevo Window Title");
-    });
-    a.run().unwrap();
+    let mut noise;
+    let mut carry;
+    let mut seed = 0xbeefu32;
+
+    let mut buffer = vec![0u32; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Noise Test - Press ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            resize: true,
+            scale_mode: ScaleMode::UpperLeft,
+            ..WindowOptions::default()
+        },
+    )
+    .expect("Unable to create the window");
+
+    window.set_target_fps(60);
+
+    let mut size = (0, 0);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let new_size = window.get_size();
+        if new_size != size {
+            size = new_size;
+            buffer.resize(size.0 * size.1, 0);
+        }
+
+        for pixel in buffer.iter_mut() {
+            noise = seed;
+            noise >>= 3;
+            noise ^= seed;
+            carry = noise & 1;
+            noise >>= 1;
+            seed >>= 1;
+            seed |= carry << 30;
+            noise &= 0xFF;
+
+            *pixel = (noise << 16) | (noise << 8) | noise;
+        }
+
+        window
+            .update_with_buffer(&buffer, new_size.0, new_size.1)
+            .unwrap();
+    }
 }
